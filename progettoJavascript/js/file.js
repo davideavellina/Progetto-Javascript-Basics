eaaf252137ff637ab2069372b51ba2294eef6d08
davideavellina <112814875+davideavellina@users.noreply.github.com>
2023-01-30 08:33:28 +0100

    Add files via upload

---
+++ progettoJavascript/js/file.js
@@ -0,0 +1,41 @@
+const counterDisplay = document.querySelector(".counterDisplay");
+const btnContainer = document.querySelector(".buttons");
+const btnPlus = document.createElement("button");
+const btnMinus = document.createElement("button");
+
+counter = 0;
+counterDisplay.textContent = counter;
+
+btnPlus.addEventListener ("click", ()=> {
+    counter++;
+    counterDisplay.textContent = counter;
+} );
+    
+btnMinus.addEventListener ("click", ()=> {
+    counter--;
+    counterDisplay.textContent = counter;
+} );
+    
+
+btnPlus.style.display = "flex";
+btnPlus.style.alignItems = "center";
+btnPlus.style.justifyContent = "center";
+btnPlus.style.width = "100px";
+btnPlus.style.height = "100px";
+btnPlus.style.backgroundColor = "gray";
+btnPlus.style.fontSize = "40px";
+btnPlus.textContent = "+";
+
+btnMinus.style.display = "flex";
+btnMinus.style.alignItems = "center";
+btnMinus.style.justifyContent = "center";
+btnMinus.style.width = "100px";
+btnMinus.style.height = "100px";
+btnMinus.style.backgroundColor = "gray";
+btnMinus.style.fontSize = "40px";
+btnMinus.textContent = "-";
+
+
+btnContainer.appendChild(btnPlus);
+btnContainer.appendChild(btnMinus);
+
